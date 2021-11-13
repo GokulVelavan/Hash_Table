0ec0a9b8ef4312786beb19f8e819dc64f7bee59c
GokulVelavan <gokulvelavans@gmail.com>
2021-11-13 12:30:38 +0530

    Frequency of the word is checked

--- Program.cs
+++ Program.cs
@@ -8,9 +8,39 @@ namespace Hash_Table
 {
     class Program
     {
+        //static method to count frequency of word
+        public static void CountWordFrequency(string sentence)
+        {
+            //declaring datatype for key and value
+            MapNode<string, int> hashtable = new MapNode<string, int>(10);
+            
+          
+           
+
+            //spliting the sentence to generate keys
+            string[] words = sentence.Split();
+
+            foreach (string word in words)
+            {
+                if (hashtable.Exists(word))
+                {
+                    //if it exist then get value of linkedList object
+                    hashtable.Add(word, hashtable.Get(word) + 1);
+                }
+                else
+                {
+                    hashtable.Add(word, 1);
+                }
+            }
+            Console.WriteLine("Displaying after add operation\n");
+            hashtable.Display();
+            Console.WriteLine("-----------------------------------------");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to hash table");
+            Console.WriteLine("Welcome To Hash Table");
+            string wordInSentence = "hello world hello world";
+            CountWordFrequency(wordInSentence);
             Console.ReadLine();
         }
     }
